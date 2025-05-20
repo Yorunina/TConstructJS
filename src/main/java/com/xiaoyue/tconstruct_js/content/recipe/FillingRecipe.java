bf373e068863dc8488c8c291d82f564a6a3c1f4f
Qi-Month <dy757410@163.com>
2025-05-20 22:40:30 +0800

    修改配方结构, 使得可以使用构造方法进行参数表示

--- src/main/java/com/xiaoyue/tconstruct_js/content/recipe/FillingRecipe.java
+++ src/main/java/com/xiaoyue/tconstruct_js/content/recipe/FillingRecipe.java
@@ -7,11 +7,9 @@ import dev.latvian.mods.kubejs.recipe.component.NumberComponent;
 import dev.latvian.mods.kubejs.recipe.schema.RecipeSchema;
 
 public interface FillingRecipe {
-
     RecipeKey<Integer> FLUID_AMOUNT = NumberComponent.INT.key("fluid_amount").optional(100);
-    RecipeKey<InputItem> CONTAINER = ItemComponents.INPUT.key("container");
+    RecipeKey<InputItem> CONTAINER = ItemComponents.INPUT.key("container").optional(InputItem.EMPTY);
 
     RecipeSchema TABLE_FILLING = new RecipeSchema(FLUID_AMOUNT, CONTAINER);
     RecipeSchema BASIN_FILLING = new RecipeSchema(FLUID_AMOUNT, CONTAINER);
-
 }
